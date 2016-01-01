b9e03f2fa367cfecd425b1144b1d7a74ab90a57c
rahulsahay19 <rahulsahay19@gmail.com>
2016-01-01 19:46:49 +0530

    3rd Commit

---
+++ MVC-Life-Cycle/CustomModules/CustomModule.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using System.Web;
+
+namespace MVC_Life_Cycle.CustomModules
+{
+    public class CustomModule : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            context.LogRequest += Context_LogRequest;
+        }
+
+        private void Context_LogRequest(object sender, System.EventArgs e)
+        {
+            Debug.WriteLine("Item Logged Successfully");
+        }
+
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- MVC-Life-Cycle/Global.asax.cs
+++ MVC-Life-Cycle/Global.asax.cs
@@ -1,16 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
+﻿using System.Diagnostics;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using MVC_Life_Cycle;
+using MVC_Life_Cycle.CustomModules;
 
+[assembly: PreApplicationStartMethod(typeof(MvcApplication), "RegisterCustomModule")]
 namespace MVC_Life_Cycle
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        public static void RegisterCustomModule()
+
+        {
+            HttpApplication.RegisterModule(typeof(CustomModule));
+        }
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
