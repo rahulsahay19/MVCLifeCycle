805aedfc65e5be3c1e6c527544f853cc5359df97
rahulsahay19 <rahulsahay19@gmail.com>
2016-01-02 21:16:45 +0530

    4th Commit

--- MVC-Life-Cycle/App_Start/RouteConfig.cs
+++ MVC-Life-Cycle/App_Start/RouteConfig.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.DynamicData;
 using System.Web.Mvc;
 using System.Web.Routing;
+using MVC_Life_Cycle.CustomHandlers;
 
 namespace MVC_Life_Cycle
 {
@@ -13,11 +15,22 @@ namespace MVC_Life_Cycle
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.Add(new Route("home", new NewHandler()));
+
+            //Default Route is very greedy by nature. This will match almost any route. Hence, you need to put the new route above this.
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+        }
+    }
+
+    public class NewHandler : IRouteHandler
+    {
+        public IHttpHandler GetHttpHandler(RequestContext requestContext)
+        {
+            return new CustomHandler();
         }
     }
 }
---
+++ MVC-Life-Cycle/CustomHandlers/CustomHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web;
+
+namespace MVC_Life_Cycle.CustomHandlers
+{
+    public class CustomHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Redirect("http://myview.rahulnivi.net");
+        }
+
+        //This is also correct
+        //public bool IsReusable
+        //{
+        //    get { return false;}
+        //}
+
+        //But I have used expression syntax here
+        public bool IsReusable => false;
+    }
+}
